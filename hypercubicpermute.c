7d42e5bb644df5d3454e4991f1c797cc8d2d4023
Yatin Singla <yatinsingla2@gmail.com>
2020-10-26 22:47:33 -0700

    Implement MyAllReduce NaiveReduce MPILibraryReduce

--- hypercubicpermute.c
+++ hypercubicpermute.c
@@ -20,21 +20,22 @@
 #include <mpi.h> 
 #include <assert.h>
 #include <time.h>
+#include <stdbool.h>
+#include <sys/time.h>
 
 int rank, p, n;
 
 // hypercubic permutation
 // returns global sum
-int MyAllReduce(void);
+int MyAllReduce(int * inArr, int size, MPI_Op op);
 
 // Array bus permutation
 // returns global sum
-int NaiveAllReduce(void);
+int NaiveAllReduce(int * inArr, int size, MPI_Op op);
 
 // performs STD MPI REDUCE
 // returns global sum
-int MPILibraryAllReduce(void);
-
+int MPILibraryAllReduce(int * inArr, int size, MPI_Op op);
 
 // generates an array of size n/p of random ints
 // returns memory address of the newly created array 
@@ -44,7 +45,7 @@ int * GenerateArray(int size);
 // to perform reduction
 // returns 1 if correctness test fails
 // returns 0 for pass
-int testCorrectness(void);
+bool testCorrectness(void);
 
 // compares runtime performance for all three reduce functions
 // and generates a log file
@@ -55,7 +56,7 @@ void printArray(int * inArr, int size);
 
 int main(int argc,char *argv[]) {
 
-    // Initialize MPI
+   // Initialize MPI
    MPI_Init(&argc,&argv);
    MPI_Comm_rank(MPI_COMM_WORLD,&rank);
    MPI_Comm_size(MPI_COMM_WORLD,&p);
@@ -73,9 +74,18 @@ int main(int argc,char *argv[]) {
 
    // precondition is divisible by p
    assert(n%p==0);
+   
+   /*
+   bool isComplete = true;
+
+   isComplete = testCorrectness();
   
-   int * arr = GenerateArray(n/p);
-   printArray(arr, n/p);   
+   if (rank == 0) {
+        printf("isComplete = %d", isComplete);
+   } 
+   */
+
+   pComparison();
 
    MPI_Finalize();
 }
@@ -96,6 +106,82 @@ int * GenerateArray(int size){
     return a;
 }
 
+int MyAllReduce(int * inArr, int size, MPI_Op op) {
+
+   int localMax = inArr[0];
+   int localSum = 0;
+   if (op == MPI_SUM) {
+      //2. compute local sum
+      for(int i = 0; i < size; i++) {
+      	localSum += inArr[i];
+      }
+      if (p == 1) {
+        printf("Rank=%d: MyAllReduce Global Sum = %d\n",rank,localSum);
+        return localSum;
+      }
+   }
+   // else op == MPI_MAX
+   else {
+     for (int i = 0; i < size; i++) {
+         if (inArr[i] > localMax) {
+             localMax = inArr[i];
+         }
+     }
+     if (p == 1) {
+        printf("Rank=%d: MyAllReduce Global Max = %d\n",rank,localMax);
+        return localMax;
+     }
+   }
+   
+   //3. compute sum in parallel
+   int globalSum = localSum, receivedSum = 0;
+   int globalMax = localMax, receivedMax = 0;
+   int temp = 1, iter = 0;
+
+   // : there will be lg p iterations
+   for(int step = 1; step < p; step *= 2) {
+   		if(rank==0) {
+			printf("Info: Iteration #%d\n",iter);
+			//fflush(stdout);
+		}
+   	
+        // : during iteration i, rank j will communicate with rank j'
+        // :			iff the i^th lsb(j) <is *not* equal to> i^th lsb(j')
+   		int mate = rank^temp;  
+	    //	printf("Rank=%d: Mate =%d\n",rank,mate);
+	    //	fflush(stdout);
+
+		MPI_Status stat;
+        if (op == MPI_SUM) {
+            // : for the individual communications let us use the MPI_Sendrecv primitive
+		    MPI_Sendrecv(&globalSum, 1, MPI_INT, mate, 0, &receivedSum, 1, MPI_INT,\
+		    			 mate, 0, MPI_COMM_WORLD, &stat);
+		    globalSum += receivedSum;
+        }
+        else {
+		    MPI_Sendrecv(&globalMax, 1, MPI_INT, mate, 0, &receivedMax, 1, MPI_INT,\
+		    			 mate, 0, MPI_COMM_WORLD, &stat);
+            if (receivedMax > globalMax) {
+                globalMax = receivedMax;
+            }
+        }
+	    //printf("Rank=%d: iter<%d>: rec'd message from %d\n",rank,iter,stat.MPI_SOURCE);
+	    //fflush(stdout);
+		temp = temp<<1;
+		iter++;
+
+		MPI_Barrier(MPI_COMM_WORLD);
+   }
+  
+  if (op == MPI_SUM) { 
+      printf("Rank=%d: MyAllReduce Global Sum = %d\n",rank,globalSum);
+      return globalSum;
+  }
+  else {
+      printf("Rank=%d: MyAllReduce Global Max = %d\n",rank,globalMax);
+      return globalMax; 
+  }
+}
 
 void printArray(int * inArr, int size) {
      printf("Rank = %d\n", rank);
@@ -106,4 +192,210 @@ void printArray(int * inArr, int size) {
      } 
      printf("|\n\n");
      return;
+}
+
+int NaiveAllReduce(int * inArr, int size, MPI_Op op) {
+    int source = rank -1, dest = rank +1; 
+    MPI_Status status;
+    if (op == MPI_SUM) {
+        int localSum = 0;
+        //2. compute local sum
+        for(int i = 0; i < size; i++) {
+       	    localSum += inArr[i];
+        }
+        int globalSum = localSum, receivedSum = 0;
+    
+        if (rank == 0) {
+            dest = dest % p;
+            //MPI send localMax
+            MPI_Send(&globalSum, 1, MPI_INT, dest, rank, MPI_COMM_WORLD);
+            //recv Global max after computation
+            MPI_Recv(&receivedSum, 1, MPI_INT, dest, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            globalSum = receivedSum;
+        }
+        else if (rank > 0 && rank < p-1) { 
+            // MPI recv and send
+            MPI_Recv(&receivedSum, 1, MPI_INT, source, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            globalSum += receivedSum;
+            MPI_Send(&globalSum, 1, MPI_INT, dest, rank, MPI_COMM_WORLD);
+
+
+            //recv globalMax after computation
+            MPI_Recv(&receivedSum, 1, MPI_INT, dest, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            globalSum = receivedSum;
+            //relay the information
+            MPI_Send(&globalSum, 1, MPI_INT, source, rank, MPI_COMM_WORLD);
+        }
+        // rank == p-1
+        else {
+            source = source % p;
+            // MPI recv
+            MPI_Recv(&receivedSum, 1, MPI_INT, source, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            globalSum += receivedSum;
+            // relay the globalMax
+            MPI_Send(&globalSum, 1, MPI_INT, source, rank, MPI_COMM_WORLD);
+        }
+        printf("Rank=%d: Naive Reduce Global Sum = %d\n",rank,globalSum);
+        return globalSum;
+    }
+    // else op == MPI_MAX
+    else {
+        int localMax = inArr[0];
+        for (int i = 0; i < size; i++) {
+              if (inArr[i] > localMax) {
+                  localMax = inArr[i];
+            }
+        }
+        int globalMax = localMax, receivedMax = 0;
+
+        if (rank == 0) {
+            dest = dest % p;
+            //MPI send localMax
+            MPI_Send(&globalMax, 1, MPI_INT, dest, rank, MPI_COMM_WORLD);
+            //recv Global max after computation
+            MPI_Recv(&receivedMax, 1, MPI_INT, dest, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            globalMax = receivedMax;
+        }
+        else if (rank > 0 && rank < p-1) { 
+            // MPI recv and send
+            MPI_Recv(&receivedMax, 1, MPI_INT, source, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            if (receivedMax > globalMax) {
+                globalMax = receivedMax;
+            }
+            MPI_Send(&globalMax, 1, MPI_INT, dest, rank, MPI_COMM_WORLD);
+
+
+            //recv globalMax after computation
+            MPI_Recv(&receivedMax, 1, MPI_INT, dest, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            globalMax = receivedMax;
+            //relay the information
+            MPI_Send(&globalMax, 1, MPI_INT, source, rank, MPI_COMM_WORLD);
+        }
+        // rank == p-1
+        else {
+            source = source % p;
+            // MPI recv
+            MPI_Recv(&receivedMax, 1, MPI_INT, source, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            if (receivedMax > globalMax) {
+                globalMax = receivedMax;
+            }
+            // relay the globalMax
+            MPI_Send(&globalMax, 1, MPI_INT, source, rank, MPI_COMM_WORLD);
+        }
+
+        printf("Rank=%d: Naive Reduce Global Max = %d\n",rank,globalMax);
+        return globalMax;
+    }
+}
+
+int MPILibraryAllReduce(int * inArr, int size, MPI_Op op) {
+  if (op == MPI_MAX) {
+        // compute localMax 
+        int localMax = inArr[0];
+        for (int i = 0; i < size; i++) {
+            if (inArr[i] > localMax) {
+                localMax = inArr[i];
+            }
+        }
+        int globalMax = localMax;
+        int receivedMax = localMax;
+        MPI_Allreduce(&localMax, &receivedMax, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
+        if (receivedMax > globalMax) {
+            globalMax = receivedMax;
+        }
+        printf("Rank=%d: MPI Library All Reduce Global Max = %d\n",rank,globalMax);
+        return globalMax;
+  }
+  // op == MPI_SUM
+  else {
+      //2. compute local sum
+      int localSum = 0;
+      for(int i = 0; i < size; i++) {
+      	localSum += inArr[i];
+      }
+      int globalSum = 0;
+      //3. compute sum in parallel
+      int receivedSum = 0;
+      MPI_Allreduce(&localSum, &receivedSum, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+      globalSum += receivedSum;
+      printf("Rank=%d: MPI Library All Reduce Global Sum = %d\n",rank,globalSum);
+      return globalSum;  
+  }
+}
+
+
+bool testCorrectness(void) {
+
+   int MyAllRedGS = 0, MyAllRedGM = 0;
+   int NaiveAllRedGS = 0, NaiveAllRedGM = 0;
+   int MPILibAllRedGS = 0, MPILibAllRedGM = 0;
+
+   int * arr = GenerateArray(n/p);
+   printArray(arr, n/p);   
+
+   MyAllRedGS = MyAllReduce(arr, n/p, MPI_SUM);
+   MyAllRedGM = MyAllReduce(arr, n/p, MPI_MAX);
+   NaiveAllRedGS = NaiveAllReduce(arr, n/p, MPI_SUM);
+   NaiveAllRedGM = NaiveAllReduce(arr, n/p, MPI_MAX);
+   MPILibAllRedGS = MPILibraryAllReduce(arr, n/p, MPI_SUM);
+   MPILibAllRedGM = MPILibraryAllReduce(arr, n/p, MPI_MAX);
+
+   
+   if (rank == 0 ) { 
+        printf("My All Reduce Global Sum = %d\n", MyAllRedGS);
+        printf("My All Reduce Global Max = %d\n", MyAllRedGM);
+        printf("Naive All Reduce Global Sum = %d\n", NaiveAllRedGS);
+        printf("Naive All Reduce Global Max = %d\n", NaiveAllRedGM);
+        printf("MPI Library All Reduce Global Sum = %d\n", MPILibAllRedGS);
+        printf("MPI Library All Reduce Global Max = %d\n", MPILibAllRedGM);
+
+        if ((MyAllRedGS == NaiveAllRedGS) && (MyAllRedGS == MPILibAllRedGS) && (MyAllRedGM == NaiveAllRedGM) && (MyAllRedGM == MPILibAllRedGM)) {
+             printf("True\n");
+             return true;
+        }
+   }
+
+   return false;
+   
+}
+
+void pComparison(void) {
+    FILE * outfile = fopen("log.csv", "a");
+    int myReduce = 0, naiveReduce = 0, mpiReduce = 0;
+    double myReduceRuntime = 0.0, naiveReduceRuntime = 0.0, mpiReduceRuntime = 0.0;
+    struct timeval myReduceStart, myReduceStop, naiveReduceStart, naiveReduceStop, mpiReduceStart, mpiReduceStop;
+
+    int * a = NULL;
+
+    /*
+    if (rank == 0) {
+        fprintf(outfile, "p, N, MyAllReduce, NaiveReduce, MPILibraryReduce\n");
+    }
+    */
+
+    a = GenerateArray(n/p);
+    
+    gettimeofday(&myReduceStart, NULL);
+    MyAllReduce(a, n/p, MPI_SUM);
+    gettimeofday(&myReduceStop, NULL);
+    myReduceRuntime = (myReduceStop.tv_sec - myReduceStart.tv_sec)*1000000 + (myReduceStop.tv_usec - myReduceStart.tv_usec);
+
+    gettimeofday(&naiveReduceStart, NULL);
+    NaiveAllReduce(a, n/p, MPI_SUM);
+    gettimeofday(&naiveReduceStop, NULL);
+    naiveReduceRuntime = (naiveReduceStop.tv_sec - naiveReduceStart.tv_sec)*1000000 + (naiveReduceStop.tv_usec - naiveReduceStart.tv_usec);
+
+    gettimeofday(&mpiReduceStart, NULL);
+    MPILibraryAllReduce(a, n/p, MPI_SUM);
+    gettimeofday(&mpiReduceStop, NULL);
+    mpiReduceRuntime = (mpiReduceStop.tv_sec - mpiReduceStart.tv_sec)*1000000 + (mpiReduceStop.tv_usec - mpiReduceStart.tv_usec);
+    
+    if (rank == 0) {
+        // N in KB
+        fprintf(outfile, "%d, %d, %lf, %lf, %lf\n", p, (int)((n*4)/1024), myReduceRuntime, naiveReduceRuntime, mpiReduceRuntime);
+    }
+    free(a);
+
+    fclose(outfile);
+    return;
 }
---
+++ performanceScript.sh
@@ -0,0 +1,152 @@
+#!/bin/bash
+
+# N = 256 INT == 1KB
+# N = 262144 INT = 1MB
+./processJob.sh 1 1 256
+sleep 3
+./processJob.sh 1 1 512
+sleep 3
+./processJob.sh 1 1 1024
+sleep 3
+./processJob.sh 1 1 2048
+sleep 3
+./processJob.sh 1 1 4096
+sleep 3
+./processJob.sh 1 1 8192
+sleep 3
+./processJob.sh 1 1 16384
+sleep 3
+./processJob.sh 1 1 32768
+sleep 3
+./processJob.sh 1 1 65536
+sleep 3
+./processJob.sh 1 1 131072
+sleep 3
+./processJob.sh 1 1 262144
+sleep 3
+
+#./processJob.sh 1 2 256
+#sleep 3
+#./processJob.sh 1 2 512
+#sleep 3
+#./processJob.sh 1 2 1024
+#sleep 3
+#./processJob.sh 1 2 2048
+#sleep 3
+#./processJob.sh 1 2 4096
+#sleep 3
+#./processJob.sh 1 2 8192
+#sleep 3
+#./processJob.sh 1 2 16384
+#sleep 3
+#./processJob.sh 1 2 32768
+#sleep 3
+#./processJob.sh 1 2 65536
+#sleep 3
+#./processJob.sh 1 2 131072
+#sleep 3
+#./processJob.sh 1 2 262144
+#sleep 3
+#
+#./processJob.sh 1 4 256
+#sleep 3
+#./processJob.sh 1 4 512
+#sleep 3 
+#./processJob.sh 1 4 1024
+#sleep 3
+#./processJob.sh 1 4 2048
+#sleep 3
+#./processJob.sh 1 4 4096
+#sleep 3
+#./processJob.sh 1 4 8192
+#sleep 3
+#./processJob.sh 1 4 16384
+#sleep 3
+#./processJob.sh 1 4 32768
+#sleep 3
+#./processJob.sh 1 4 65536
+#sleep 3
+#./processJob.sh 1 4 131072
+#sleep 3
+#./processJob.sh 1 4 262144
+#sleep 3
+#
+#./processJob.sh 1 8 256
+#sleep 3
+#./processJob.sh 1 8 512
+#sleep 3
+#./processJob.sh 1 8 1024
+#sleep 3
+#./processJob.sh 1 8 2048
+#sleep 3
+#./processJob.sh 1 8 4096
+#sleep 3
+#./processJob.sh 1 8 8192
+#sleep 3
+#./processJob.sh 1 8 16384
+#sleep 3
+#./processJob.sh 1 8 32768
+#sleep 3
+#./processJob.sh 1 8 65536
+#sleep 3
+#./processJob.sh 1 8 131072
+#sleep 3
+#./processJob.sh 1 8 262144
+#sleep 3
+#
+#./processJob.sh 2 16 256
+#sleep 3
+#./processJob.sh 2 16 512
+#sleep 3
+#./processJob.sh 2 16 1024
+#sleep 3
+#./processJob.sh 2 16 2048
+#sleep 3
+#./processJob.sh 2 16 4096
+#sleep 3
+#./processJob.sh 2 16 8192
+#sleep 3
+#./processJob.sh 2 16 16384
+#sleep 3
+#./processJob.sh 2 16 32768
+#sleep 3
+#./processJob.sh 2 16 65536
+#sleep 3
+#./processJob.sh 2 16 131072
+#sleep 3
+#./processJob.sh 2 16 262144
+#sleep 3
+#
+#./processJob.sh 4 32 256
+#sleep 3
+#./processJob.sh 4 32 512
+#sleep 3
+#./processJob.sh 4 32 1024
+#sleep 3
+#./processJob.sh 4 32 2048
+#sleep 3
+#./processJob.sh 4 32 4096
+#sleep 3
+#./processJob.sh 4 32 8192
+#sleep 3
+#./processJob.sh 4 32 16384
+#sleep 3
+#./processJob.sh 4 32 32768
+#sleep 3
+#./processJob.sh 4 32 65536
+#sleep 3
+#./processJob.sh 4 32 131072
+#sleep 3
+#./processJob.sh 4 32 262144
+
+#./processJob.sh 8 32 256
+#./processJob.sh 8 32 512
+#./processJob.sh 8 32 1024
+#./processJob.sh 8 32 2048
+#./processJob.sh 8 32 4096
+#./processJob.sh 8 32 8192
+#./processJob.sh 8 32 16384
+#./processJob.sh 8 32 32768
+#./processJob.sh 8 32 65536
+#./processJob.sh 8 32 131072
+#./processJob.sh 8 32 262144
